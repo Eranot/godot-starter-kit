61733115d5eb42ecbde43e8554c0f31f3b014d4b
Renato MK <renato.konflanz@unochapeco.edu.br>
2022-12-29 00:20:01 -0300

    Adding splash screen

---
+++ scenes/menu/splash_screen/splash_screen.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export_file("*.tscn") var menu_scene_file
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(2).timeout
+	get_tree().create_tween().tween_property(%GodotScreen, "modulate", Color.hex(0xffffffff), 1)
+	await get_tree().create_timer(3).timeout
+	get_tree().create_tween().tween_property(%GodotScreen, "modulate", Color.hex(0xffffff00), 1)
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file(menu_scene_file)
