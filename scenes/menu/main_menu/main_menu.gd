a18381ba280ead0a1a662113dab95f23c76ad123
Renato MK <renato.konflanz@unochapeco.edu.br>
2024-01-14 09:50:22 -0300

    Updating fot 4.2.1

--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -4,19 +4,21 @@ extends Control
 @export_file("*.tscn") var options_scene_file
 @export_file("*.tscn") var credits_scene_file
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	MusicPlayer.play()
-#	if(!SoundController.get_music_player().is_playing):
-#		SoundController.get_music_player().play_sound("empty-mind.mp3")
+	if not MusicPlayer.playing:
+		MusicPlayer.play()
+
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file(play_scene_file)
+
 
 func _on_options_button_pressed():
 	var scene = load(options_scene_file).instantiate()
 	scene.show_buttons = false
 	add_child(scene)
+
 
 func _on_credits_button_pressed():
 	get_tree().change_scene_to_file(credits_scene_file)
